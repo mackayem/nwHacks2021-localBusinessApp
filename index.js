9cbfc37191793e799bc8ac611312c48a01b0bd39
SalinaK1 <saleena.koirala1@gmail.com>
2021-01-10 11:15:32 +0545

    Getting user's location API implemented.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
  const fetch = require('node-fetch');
  const helmet = require('helmet'); //security for the API
  const bodyParser = require('body-parser');
+ const unirest = require("unirest") ;
 
 //express object
 const app = express();
@@ -19,7 +20,36 @@ const port = process.env.PORT || 4001;
 //http requests
 //GETS to front page
 app.get('/', (req, res) => {
-    res.send('server working!');
+    // res.send('server working!');
+    const apiCall = unirest(
+
+        "GET",
+     
+        "https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/"
+     
+      );
+    //   apiCall.query({
+    //     "ip": "yourIP address"
+    //     });
+     
+      apiCall.headers({
+     
+        "x-rapidapi-host": "ip-geolocation-ipwhois-io.p.rapidapi.com",
+     
+        "x-rapidapi-key": "b5871293aamsh822f2e9c47063aep14f351jsn4da519d69c4a"
+     
+      });
+     
+      apiCall.end(function(result) {
+     
+        if (res.error) throw new Error(result.error);
+     
+        console.log(result.body);
+     
+        res.send(result.body);
+     
+      });
+    
 });
 
 
