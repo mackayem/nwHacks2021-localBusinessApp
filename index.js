1fb1b7b117d1cbc6df1bb7e11d068187c691c3b3
Phil Fiess <pfiess@protonmail.com>
2021-01-10 12:36:03 -0800

    basic back end logic. untested because front end form needs implementing

--- index.js
+++ index.js
@@ -8,13 +8,17 @@
  const fetch = require('node-fetch');
  const helmet = require('helmet'); //security for the API
  const bodyParser = require('body-parser');
- const SerpApi = require('google-search-results-nodejs');
+ const serpApi = require('google-search-results-nodejs');
+
 
  //env vars
  require('dotenv').config();
 
  //express object
 const app = express();
+app.use(express.static('front-end'));
+app.use(express.json({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //port automatically defaults to 4001 if not rendered by process environment
 const port = process.env.PORT || 4001;
@@ -22,59 +26,99 @@ const port = process.env.PORT || 4001;
 
 //http requests
 //GETS to front page
-app.get('/weather:currentLocation', async (req, res) => {
-      console.log(req.body);
-      const openWeatherAPIKEY = process.env.OPEN_WEATHER_MAP_API_KEY;
-      const weatherMapUrl = `https://api.openweathermap.org/data/2.5/weather?q=vancouver&appid=${openWeatherAPIKEY}`;
-      const openWeatherResponse = await fetch(weatherMapUrl).catch(e => { console.log(e) });
-      const openWeatherData = await openWeatherResponse.json().catch(e => { console.log(e) });
-      console.log(openWeatherData);
-      const weatherData = {
-          weather: openWeatherData,
-      }
-      res.send(weatherData);
-    });
-app.get('/listing', async (req, res) => {
-        const openWeatherAPIKEY = process.env.GOOGLE_SEARCH_API_KEY;
-        const search = new SerpApi.GoogleSearch(`${googleSearchAPI}`);
-        const lat = req.body.latitude;
-        const lng = req.body.longitude;
-        const params = {
-        engine: "google",
-        q: "school",
-        ll: "%4040.7455096%2C-74.0083012%2C15.1z",
-        type: "search",
-        google_domain: "google.com",
-        num: "5"
+
+app.get('/', async (req, res) => {
+    //geolocator
+    //TODO: implement city locator
+    // const city = request.params.city;
+    
+    //weather data
+    const openWeatherAPIKEY = process.env.OPEN_WEATHER_MAP_API_KEY;
+    const weatherMapUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${openWeatherAPIKEY}`;
+    const openWeatherData = await openWeatherResponse.json().catch(e => { console.log(e) });
+    //weather main data
+    const weatherData = {
+        weatherCode: openWeatherData.weather.id,
+        weatherConditions: openWeatherData.weather.main
+    };
+    
+    let isWeatherDecent = true;
+    //all good weather codes are codes, 800-804. All else are 
+    //inclement. use outdoor and indoor activities accordingly
+    if (weatherData.weatherCode != 800 ||
+        weatherData.weatherCode != 801 ||
+        weatherData.weatherCode != 802 ||
+        weatherData.weatherCode != 803 ||
+        weatherData.weatherCode != 804) {
+            isWeatherDecent = false;
+        }
+
+    //indoor activities, shopping, restaurants nearby (included both in bad weather and good)
+    const coffee = serpAPIQuery(`coffee ${city}`);
+    const bars = serpAPIQuery(`bar ${city}`);
+    const museums = serpAPIQuery(`museums ${city}`);
+    const galleries = serpAPIQuery(`gallery ${city}`);
+    const movieTheaters = serpAPIQuery(`movie theaters ${city}`);
+    const shopping = serpAPIQuery(`shopping ${city}`);
+    const restauraunts = serpAPIQuery(`restaurants ${city}`);
+
+    const indoorActivities = {
+        coffee: coffee,
+        bars: bars,
+        museums: museums,
+        galleries: galleries,
+        movieTheaters: movieTheaters,
+        shopping: shopping,
+        restaurants: restauraunts
+    };
+
+    if (isWeatherDecent) {
+        //outdoor activities like amusement parks, parks, beaches, etc
+        const beaches = serpAPIQuery(`beach ${city}`);
+        const parks = serpAPIQuery(`park ${city}`);
+        const hikingTrails = serpAPIQuery(`hiking trails ${city}`);
+
+        const outdoorActivities = {
+            beaches: beaches,
+            parks: parks,
+            hikingTrails: hikingTrails
         };
 
-        const callback = function(data) {
+        const activities = {
+            indoor: indoorActivities,
+            outdoor: outdoorActivities
+        };
+    } else {
+        const activities = {
+            indoor: indoorActivities //rainy day activities
+        }
+    }
+
+    //return all available activities
+    res.json(activities);
+
+});
+
+//this is a function to query the serpAPI from google for local businesses
+function serpAPIQuery(q){
+    const googleSearchAPIKey = process.env.GOOGLE_SEARCH_API_KEY;
+    const search = new serpApi.GoogleSearch(`${googleSearchAPIKey}`);
+    const params = {
+        engine: 'google',
+        q: q,
+        type: 'search',
+        google_domain: 'google.com',
+        num: '5',
+    };
+    const callback = function(data) {
         console.log(data['local_results']);
         };
 
-        // Show result as JSON
-        const list = { result: search.json(params, callback), }
-        res.send(list);
-});
-app.get('/address', async (req, res) => {
-        const lat = req.body.latitude;
-        const lng = req.body.longitude;
-            const locationAPIKey = 'process.env.LOCATION_API_KEY';
-            var datas;
-            const locationURL = `http://api.positionstack.com/v1/reverse?access_key=${locationAPIKey}&query=${lat},${lng}&limit=10&output=json`;
-            const locationResponse = await fetch(locationURL).catch(e => { console.log(e) });
-            const openlocationData = await locationResponse.json().catch(e => { console.log(e) });
-            for(datas = 0; datas < 10; datas++){
-                console.log(openlocationData.data[datas].name, openlocationData.data[datas].country);
-            }
-            const locationData = {
-                location: openlocationData,
-            }
-            console.log(locationData)
-            res.send(locationData)
+    // Show result as JSON
+    const list = { result: search.json(params, callback), }
 
-});
-
+    return list;
+}
 //app listening to port
 app.listen(port, () => {
     console.log('Listening on port ' + port);
