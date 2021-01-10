837e46779888197f65ec1469777cd2fa308d984d
SalinaK1 <saleena.koirala1@gmail.com>
2021-01-10 18:11:21 +0545

    API to find out user's location tested and working.

---
+++ location.js
@@ -0,0 +1,25 @@
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(displayLocationInfo);
+
+        function displayLocationInfo(position) {
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+        getCurrentAddress(lat, lng);
+        console.log(`longitude: ${ lng } | latitude: ${ lat }`);
+        }
+        async function getCurrentAddress(lat, lng){
+            const locationAPIKey = process.env.LOCATION_API_KEY;
+            var datas;
+            const locationURL = `http://api.positionstack.com/v1/reverse?access_key=${locationAPIKey}&query=${lat},${lng}&limit=10&output=json`;
+            const locationResponse = await fetch(locationURL).catch(e => { console.log(e) });
+            const openlocationData = await locationResponse.json().catch(e => { console.log(e) });
+            for(datas = 0; datas < 10; datas++){
+                console.log(openlocationData.data[datas].name, openlocationData.data[datas].country);
+            }
+            const locationData = {
+                location: openlocationData,
+            }
+            console.log(locationData);
+        }
+        
+}
