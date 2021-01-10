5d87bca491d6ba1801aafc97639753dc6ec22ca0
Phil Fiess <pfiess@protonmail.com>
2021-01-09 18:32:16 -0800

    sets up express server

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('finally working!')
+/**
+ * Back end for Local Business App
+ * built for nwHacks 2021
+ */
+
+ //dependencies
+ const express = require('express');
+ const fetch = require('node-fetch');
+ const helmet = require('helmet'); //security for the API
+ const bodyParser = require('body-parser');
+
+//express object
+const app = express();
+
+//port automatically defaults to 4001 if not rendered by process environment
+const port = process.env.PORT || 4001;
+
+
+//http requests
+//GETS to front page
+app.get('/', (req, res) => {
+    res.send('server working!');
+});
+
+
+
+//app listening to port
+app.listen(port, () => {
+    console.log('Listening on port ' + port);
+});
