177b212fb6f3fb802ce121c9a441ed52f888e1ba
Phil Fiess <pfiess@protonmail.com>
2021-01-10 12:35:28 -0800

    front end cleanup in progress

--- front-end/scripts.js
+++ front-end/scripts.js
@@ -1,32 +1,3 @@
-function include(file) { 
-
-    var script = document.createElement('script'); 
-    script.src = file; 
-    script.type = 'text/javascript'; 
-    script.defer = true; 
-    
-    document.getElementsByTagName('head').item(0).appendChild(script); 
-    
-    } 
-
-include('../index.js'); 
-
-// async function getWeather() {
-//     const res = await fetch(`/weather`)
-//     const data = await res.json() 
-//     return data
-// }
-// async function getLocation() {
-//     const res = await fetch(`/location`)
-//     const data = await res.json() 
-//     return data
-// }
-// async function getAddress() {
-//     const res = await fetch(`/address`)
-//     const data = await res.json() 
-//     return data
-// }
-
 //Get user location
 let lat, lon;
 if ('geolocation' in navigator) {
@@ -42,14 +13,24 @@ if ('geolocation' in navigator) {
                 longitude: lon
             };
             
-            //get weather
-            const weatherAPIURL = `weather/${lat},${lon}`;
-            const weatherResponse = await fetch(api_url);
+            // //get weather
+            // const weatherAPIURL = `/weather/${lat},${lon}`;
+            // const weatherResponse = await fetch(weatherAPIURL);
             
+
+
         } catch (e) {
             console.error(e);
         }
 
+        const data = {lat, lon}
+        const options = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        };
         
     });
 } else {
